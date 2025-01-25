d9188b4bca5b4525110491e9e65ad2eb85345923
glassedgamer <glassedgamer05@gmail.com>
2025-01-25 16:31:23 -0500

    Player Movement
    YAYYYY FINALLY

--- Assets/Scripts/Player/Player1Movement.cs
+++ Assets/Scripts/Player/Player1Movement.cs
@@ -5,15 +5,57 @@ using UnityEngine;
 public class Player1Movement : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Rigidbody2D rb;
 
-    // Update is called once per frame
+    Vector2 movement;
+
+    [SerializeField] float speed = 3f;
+    [SerializeField] float jumpForce = 10f;
+    private float moveInput;
+
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    bool isGrounded;
+
     void Update()
     {
-        
+        movement = Vector2.zero;
+
+        PlayerInput();
+    }
+
+
+    void PlayerInput()
+    {
+        moveInput = 0f;
+        if (Input.GetKey(KeyCode.A))
+            moveInput = -1f;
+        else if (Input.GetKey(KeyCode.D))
+            moveInput = 1f;
+
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if the player is grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw ground check radius in the scene view
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
